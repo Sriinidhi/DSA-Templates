dc7d07bb809e6dabf9e590c1db8ffd27ed814c5a
Polkam Srinidhi <102644860+Sriinidhi@users.noreply.github.com>
2024-09-15 04:56:09 +0530

    1CreateAppendPrintFrwdBwdSimpleDLL.java

---
+++ DoubleLinkedList/1CreateAppendPrintFrwdBwdSimpleDLL.java
@@ -0,0 +1,71 @@
+class Solution
+{
+  class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList {
+    private Node head;
+    private Node tail;
+
+    // Constructor
+    public DoublyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    // Add node to the end
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    // Print the list from head to tail
+    public void printForward() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    // Print the list from tail to head
+    public void printBackward() {
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.prev;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+
+        System.out.print("Forward: ");
+        list.printForward(); // Output: 1 2 3 
+
+        System.out.print("Backward: ");
+        list.printBackward(); // Output: 3 2 1 
+    }
+  }
+}
